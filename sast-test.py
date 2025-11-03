36e2c4a91fc4bd85af2832f5ff6f7e9c2ca9db79
Mewa-zip <malwa.ter46@gmail.com>
2025-11-03 20:37:02 +0100

    Create sast-test.py

---
+++ sast-test.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import request, Flask
+
+app = Flask(__name__)
+conn = sqlite3.connect('test.db')
+
+@app.route("/user")
+def get_user():
+    username = request.args.get("username")
+    query = f"SELECT * FROM users WHERE name = '{username}'"
+    cursor = conn.cursor()
+    cursor.execute(query) # SQL Injection
+    return str(cursor.fetchall())
